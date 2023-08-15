df8cf4eed314fb83c1bcef06935838b87d7523a1
tnielsen2 <trentnielsen84@gmail.com>
2023-08-15 09:55:58 -0400

    Test CI

--- .github/workflows/update_cfn.sh
+++ .github/workflows/update_cfn.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for json_file in $1; do
+file_path="./.github/workflows/changed_files"
+
+while IFS= read -r json_file; do
   echo "${json_file}" has changed.
-  # Perform your desired action here using $json_file
-done
+  # Perform your desired action here using "$json_file"
+done < "$file_path"
